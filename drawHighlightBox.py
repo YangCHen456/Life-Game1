a10ae7e3c54857a0b33d914e2b58f07d854f6ecf
YangCHen456 <903149342@qq.com>
2016-11-20 22:45:56 +0800

    Create drawHighlightBox.py

---
+++ drawHighlightBox.py
@@ -0,0 +1,3 @@
+def drawHighlightBox(boxx, boxy):#####blue biankuang
+    left, top = leftTopCoordsOfBox(boxx, boxy)
+    pygame.draw.rect(DISPLAYSURF, HIGHLIGHTCOLOR, (left - 5, top - 5, BOXSIZE + 10, BOXSIZE + 10), 4)
